1dcc8a7a6a45eda53fa1c8fc4931abfe6afbe605
Junior Alouna <j.alouna@itis-formation.com>
2026-06-23 22:27:57 +0000

    Add Terraform main.tf - Azure infrastructure

---
+++ terraform/main.tf
@@ -0,0 +1,27 @@
+# main.tf — Infrastructure Azure CYNA
+# Rédacteur : Junior Alouna — DevOps
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "cyna" {
+  name     = "rg-cyna-it"
+  location = "Germany West Central"
+}
+
+resource "azurerm_virtual_network" "cyna" {
+  name                = "vnet-cyna"
+  address_space       = ["10.30.0.0/16"]
+  location            = "Germany West Central"
+  resource_group_name = azurerm_resource_group.cyna.name
+}
